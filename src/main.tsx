0c539c0249e53de37b21fc8076f106190c7acb73
izergio98 <izergiodev@gmail.com>
2025-04-01 21:25:18 +0200

    feat: theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import theme from "./theme.ts"
+import { ThemeProvider } from "@emotion/react"
+import { CssBaseline } from "@mui/material"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
+  </React.StrictMode>
 )
