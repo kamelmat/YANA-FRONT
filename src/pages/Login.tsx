87c9abb0ecde3882bde2fcf7c72db1fc1ec46287
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-03 19:58:03 +0200

    feat: enhance Login component for responsive design and replace UserLog with LoginComponent

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,54 @@
-import UserLog from "../components/UserLog"
+import LogIn from "../components/LoginComponent"
+import { useScreenSize } from '../hooks/useScreenSize';
+import theme from '../theme';
+
+import { Box } from '@mui/material';
 
 export default function Login() {
-  return (
-    <UserLog />
-  )
+  const screenSize = useScreenSize();
+
+  console.log("screenSize", screenSize);
+
+  switch (screenSize) {
+    case "md":
+    case "lg":
+      const width = screenSize === "lg" ? "27%" : "80%";
+      return (
+        <Box sx={{ height: '100vh', width: '100vw', background: theme.gradients.gradientPurple, display: 'flex', alignItems: 'center' }}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              background: "black",
+              height: '80%',
+              width: width,
+              margin: 'auto',
+              borderRadius: '50px',
+              boxShadow: '0px 15px 20px rgba(0, 0, 0, 0.5)',
+              padding: '2em',
+            }}
+          >
+            <LogIn />
+          </Box>
+        </Box>
+      );
+    case "sm":
+    default:
+      return (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: theme.colors.blackBackground,
+            height: '100vh',
+          }}
+        >
+          <LogIn />
+        </Box>
+      );
+  }
 }
