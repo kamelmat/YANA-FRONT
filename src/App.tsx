69bd97fd219026b182d7e04dc3a9a603ad2d2a5b
elenalunadance <elenaluna.dance2023@gmail.com>
2025-04-02 17:46:42 -0700

    Configurar la navegación con react-router-dom (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import "./App.css"
 import { Typography, useTheme } from "@mui/material"
 import BottomNav from "./components/BottomNav"
+import { BrowserRouter } from "react-router-dom"
 
-function App() {
+const App: React.FC = () => {
   const theme = useTheme()
 
   return (
-    <>
-      <Typography variant="h1" sx={{ color: theme.colors.pink }}>
-        You are not Alone
-      </Typography>
-      <BottomNav></BottomNav>
-    </>
+    <BrowserRouter>
+      <>
+        <Typography variant="h1" sx={{ color: theme.colors.pink }}>
+          You are not Alone
+        </Typography>
+        <BottomNav></BottomNav>
+      </>
+    </BrowserRouter>
   )
 }
 
