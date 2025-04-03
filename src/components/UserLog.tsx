f203cf36dd13c7e68330592ac56ac99ed827b7e1
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-03 17:12:24 +0200

    feat: update UserLog component to use username instead of email and styled to match UI/UX design

--- src/components/UserLog.tsx
+++ src/components/UserLog.tsx
@@ -1,5 +1,6 @@
 import Google from '../assets/icons/Google.svg';
-import Logo from '../assets/images/Logo.webp';
+import Logo from '../../public/yana.svg';
+import Slogan from '../assets/branding/slogan.svg';
 import theme from '../theme';
 
 import { useState } from "react";
@@ -8,52 +9,31 @@ import { Box, Button, Checkbox, Link, Stack, TextField, Typography } from '@mui/
 
 export default function UserLog() {
 
-  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
   const screenSize = useScreenSize();
-  
+
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    console.log('Email:', email, 'Password:', password, 'Remember Me:', rememberMe);
   };
 
   if (screenSize !== "sm") return;
 
-
-
-  // Estilos reutilizables para TextField
-  const textFieldStyles = {
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': { borderColor: '#fff' },
-      '&:hover fieldset': { borderColor: '#fff' },
-      '&.Mui-focused fieldset': { borderColor: '#fff' },
-    },
-    '& .MuiInputBase-input': { color: '#fff' },
-    '& .MuiInputLabel-root': { color: '#fff' },
-    '& .MuiInputLabel-root.Mui-focused': { color: '#fff' },
-  };
   return (
     <Box
       sx={{
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        width: '100%',
-        maxWidth: '400px', 
-        gap: 3, 
+        justifyContent: 'center',
+        background: theme.colors.blackBackground,
+        height: '100vh',
       }}
     >
-      <img
-        src={Logo}
-        alt="Descripción de la imagen"
-        style={{
-          maxWidth: '100%',
-          height: '20dvh',
-          width: '45dvw',
-        }}
-      />
+      <img src={Logo} alt="Logo" style={{ width: '35vw', height: 'auto', marginBottom: "2rem" }} />
+      <img src={Slogan} alt="Slogan" style={{ width: '65vw', height: 'auto' }} />
 
       <Box
         component="form"
@@ -61,40 +41,100 @@ export default function UserLog() {
         noValidate
         sx={{
           width: '100%',
+          padding: '2em 1em 0 1em',
           color: '#fff',
         }}
       >
-        <Typography component="h1" variant="h6" gutterBottom align="center">
+        <Typography>
           Acceder
         </Typography>
 
-        <Stack spacing={2} direction="column">
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Correo Electrónico"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              sx={textFieldStyles}
-            />
+        <Stack spacing={2} direction="column" sx={{ marginTop: 2 }}>
+          <TextField
+            fullWidth
+            id="username"
+            label="Usuario"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                color: theme.colors.lightGray,
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightGray,
+                  borderWidth: "1px",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightBlue,
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightBlue,
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: theme.colors.lightGray,
+                "&.Mui-focused": {
+                  color: theme.colors.lightBlue,
+                },
+              },
+              "& input": {
+                backgroundColor: "transparent !important",
+              },
+              "& input:-webkit-autofill": {
+                WebkitBoxShadow: "0 0 0px 1000px transparent inset !important",
+                WebkitTextFillColor: "#fff !important",
+                transition: "background-color 9999s ease-in-out 0s !important",
+              },
+              "& input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active": {
+                WebkitBoxShadow: "0 0 0px 1000px transparent inset !important",
+                WebkitTextFillColor: "#fff !important",
+                transition: "background-color 9999s ease-in-out 0s !important",
+              },
+            }}
+          />
 
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Contraseña"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={password}
-              sx={textFieldStyles}
-            />
-          </Stack>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                color: theme.colors.lightGray,
+                "& .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightGray,
+                  borderWidth: "1px",
+                },
+                "&:hover .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightBlue,
+                },
+                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                  borderColor: theme.colors.lightBlue,
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: theme.colors.lightGray,
+                "&.Mui-focused": {
+                  color: theme.colors.lightBlue,
+                },
+              },
+              "& input": {
+                backgroundColor: "transparent !important",
+              },
+              "& input:-webkit-autofill": {
+                WebkitBoxShadow: "0 0 0px 1000px transparent inset !important",
+                WebkitTextFillColor: "#fff !important",
+                transition: "background-color 9999s ease-in-out 0s !important",
+              },
+              "& input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active": {
+                WebkitBoxShadow: "0 0 0px 1000px transparent inset !important",
+                WebkitTextFillColor: "#fff !important",
+                transition: "background-color 9999s ease-in-out 0s !important",
+              },
+            }}
+          />
+        </Stack>
 
         <Stack direction="row" justifyContent="space-between" alignItems="center">
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
@@ -106,24 +146,23 @@ export default function UserLog() {
               Recordarme
             </Typography>
           </Box>
-          <Link href="#" variant="body1" sx={{ color: '#4285F4', textDecoration: 'none' }}>
+          <Link href="#" variant="body2" sx={{ color: theme.colors.lightBlue, textDecoration: 'none' }}>
             Olvidé la contraseña
           </Link>
         </Stack>
 
-        <Stack spacing={3} direction="column" sx={{ marginTop: 5 }}>
+        <Stack spacing={1.5} direction="column" sx={{ marginTop: 4 }}>
           <Button
             type="submit"
             size="medium"
-            variant="outlined"
             fullWidth
             sx={{
-              marginTop: 3,
-              marginBottom: 2,
-              color: '#000000',
-              backgroundColor: '#f7f2fa',
-              borderRadius: theme.borders?.borderRadius,
-              textTransform: 'capitalize',
+              color: 'black',
+              backgroundColor: theme.colors.lightGray,
+              borderRadius: '35px',
+              textTransform: 'none',
+              padding: '1em',
+              fontWeight: 600,
             }}
           >
             Ingresar
@@ -136,32 +175,25 @@ export default function UserLog() {
             fullWidth
             startIcon={<img src={Google} alt="Google Icon" style={{ width: '20px', height: '20px' }} />}
             sx={{
-              marginTop: 3,
               marginBottom: 2,
               color: '#809AEE',
-              borderRadius: theme.borders?.borderRadius,
-              borderColor: '#4285F4',
-              textTransform: 'capitalize',
-              '&:hover': {
-                borderColor: '#4285F4',
-                borderWidth: 2,
-              },
-              '&:active': {
-                borderColor: '#4285F4',
-                borderWidth: 2,
-              },
+              borderRadius: '30px',
+              textTransform: 'none',
+              padding: '1em',
+              fontWeight: 600,
+              borderColor: theme.colors.lightBlue,
             }}
           >
-            Iniciar Sesión con Google
+            Iniciar sesión con Google
           </Button>
-
-          <Typography variant="body2" align="center" sx={{ mt: 2 }}>
-            ¿No tienes una cuenta?{' '}
-            <Link href="#" underline="none" sx={{ textTransform: 'none'}}>
-              ¡Regístrate!
-            </Link>
-          </Typography>
         </Stack>
+
+        <Typography variant="body2" align="center" sx={{ mt: 5 }}>
+          ¿No tienes una cuenta?{' '}
+          <Link href="#" underline="none" sx={{ textTransform: 'none', color: theme.colors.lightBlue }}>
+            Regístrate
+          </Link>
+        </Typography>
       </Box>
     </Box>
   );
