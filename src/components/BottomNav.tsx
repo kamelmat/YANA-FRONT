1b68daa052467233a59648c361783e15f37f1c01
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-04 00:59:02 +0200

    fix: update BottomNav to use currentIndex for navigation state

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -60,7 +60,6 @@ const StyledBottomNavAction = styled(BottomNavigationAction, {
 
 const BottomNav: React.FC = () => {
   const theme = useTheme() as Theme
-  const [value, setValue] = React.useState(0)
   const screenSize = useScreenSize()
   const navigate = useNavigate()
   const location = useLocation();
@@ -95,9 +94,10 @@ const BottomNav: React.FC = () => {
   if (screenSize !== "sm" || location.pathname === "/login") return
 
   const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue)
     navigate(navItems[newValue].route)
   }
+
+  const currentIndex = navItems.findIndex(item => item.route === location.pathname);
 
   return (
     <Paper
@@ -115,7 +115,7 @@ const BottomNav: React.FC = () => {
       }}
     >
       <BottomNavigation
-        value={value}
+        value={currentIndex}
         onChange={handleChange}
         showLabels
         sx={{
@@ -128,7 +128,7 @@ const BottomNav: React.FC = () => {
             key={item.label}
             label={item.label}
             icon={item.icon}
-            selected={value === index}
+            selected={currentIndex === index}
             selectedColor={item.selectedColor}
           />
         ))}
