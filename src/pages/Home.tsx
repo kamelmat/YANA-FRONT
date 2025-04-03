efdc54ff70e7bb155f66f210a990af794e217243
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-03 17:16:56 +0200

    fix: restore home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,7 @@
-import theme from '../theme';
-import UserLog from '../components/UserLog';
-import { Container } from '@mui/material';
-
 export default function Home() {
- 
-
   return (
-    <Container
-      component="main"
-      sx={{
-        backgroundColor: theme.colors?.blackBackground, // Fondo del tema
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 0,
-        paddingTop: 0, // Mantenemos el paddingTop original
-        boxSizing: 'border-box',
-      }}
-    >
-      <UserLog />
-      
-    </Container>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
