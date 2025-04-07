f2240f47daf2a716d9d26ba63bed2e3003235f0c
izergio98 <izergiodev@gmail.com>
2025-04-07 17:58:41 +0200

    fix: textos nuevos en register && eliminado(comentado) el incio de sesión con google

--- src/components/register/RegisterMethodStage.tsx
+++ src/components/register/RegisterMethodStage.tsx
@@ -1,32 +1,37 @@
-import { Typography, Link } from "@mui/material";
-import { useTranslation } from "react-i18next";
-import CustomButton from "../../commons/CommonButton";
-import Mail from "../../assets/icons/mail.svg";
-import Google from "../../assets/icons/Google.svg";
-import theme from "../../theme";
+import { Typography, Link } from "@mui/material"
+import { useTranslation } from "react-i18next"
+import CustomButton from "../../commons/CommonButton"
+import Mail from "../../assets/icons/mail.svg"
+// import Google from "../../assets/icons/Google.svg"
+import theme from "../../theme"
 
 export default function RegisterMethodStage({ onEmailClick }: { onEmailClick: () => void }) {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   return (
     <>
       <CustomButton
         text={t("register.method.useEmail")}
-        icon={<img src={Mail} alt="Mail Icon" style={{ width: '20px' }} />}
+        icon={<img src={Mail} alt="Mail Icon" style={{ width: "20px" }} />}
         onClick={onEmailClick}
       />
-      <CustomButton
+      {/* <CustomButton
         text={t("register.method.useGoogle")}
-        icon={<img src={Google} alt="Google Icon" style={{ width: '35px', height: '35px' }} />}
+        icon={<img src={Google} alt="Google Icon" style={{ width: "35px", height: "35px" }} />}
         variantType="secondary"
-        onClick={() => console.log('Google login')}
-      />
-      <Typography variant="body1" fontSize={13} align="center" sx={{ color: "#fff", fontWeight: "light" }}>
-        {t("register.method.privacyText")}{' '}
+        onClick={() => console.log("Google login")}
+      /> */}
+      <Typography
+        variant="body1"
+        fontSize={13}
+        align="center"
+        sx={{ color: "#fff", fontWeight: "light" }}
+      >
+        {t("register.method.privacyText")}{" "}
         <Link href="#" underline="none" sx={{ color: theme.colors.lightBlue }}>
           {t("register.method.privacyLink")}
         </Link>
       </Typography>
     </>
-  );
+  )
 }
