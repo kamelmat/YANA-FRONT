09cca295baa454d075c037b3562eed4c03c20c08
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-13 02:09:56 +0200

    feat: enhance login and registration hooks to set access token in Zustand store

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,12 +1,20 @@
 import { useMutation } from "@tanstack/react-query"
 import { useNavigate } from "react-router-dom"
 import { authService, LoginData } from "../services/auth"
+import { useAuthStore } from "../store/authStore"
 
 export const useLogin = () => {
   const navigate = useNavigate()
+  const setAccessToken = useAuthStore((state) => state.setAccessToken)
 
   return useMutation({
-    mutationFn: (data: LoginData) => authService.login(data),
+    mutationFn: async (data: LoginData) => {
+      const response = await authService.login(data)
+      if (response.access_token) {
+        setAccessToken(response.access_token)
+      }
+      return response
+    },
     onSuccess: () => navigate("/"),
     onError: (error) => console.log(error),
   })
--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -1,14 +1,22 @@
 import { useMutation } from "@tanstack/react-query"
 import { authService, RegisterData } from "../services/auth"
 import { useNavigate } from "react-router-dom"
+import { useAuthStore } from "../store/authStore"
 
 export const useRegister = () => {
   const navigate = useNavigate()
+  const setAccessToken = useAuthStore((state) => state.setAccessToken)
 
   return useMutation({
-    mutationFn: (data: RegisterData) => authService.register(data),
+    mutationFn: async (data: RegisterData) => {
+      const response = await authService.register(data)
+      if (response.access_token) {
+        setAccessToken(response.access_token)
+      }
+      return response
+    },
     onSuccess: () => {
-      navigate("/login")
+      navigate("/")
     },
     onError: (error: Error) => {
       console.error("Registration error:", error.message)
