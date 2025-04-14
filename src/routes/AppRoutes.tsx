8ee7158e0a6b57783343bee9d9b5c7809e2e392b
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2025-04-14 13:51:54 +0200

    feat: implement token refresh mechanism and integrate into AppRoutes for improved authentication flow (#89)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -9,20 +9,25 @@ import Contacts from "../pages/Contacts"
 import { Routes, Route } from "react-router-dom"
 import MapLayout from "./MapLayout"
 import ProtectedRoute from "../components/ProtectedRoute"
+import { useTokenRefresh } from "../hooks/useTokenRefresh"
 
-const AppRoutes: React.FC = () => (
-  <Routes>
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route element={<ProtectedRoute><MapLayout /></ProtectedRoute>}>
-      <Route path="/" element={<Home />} />
-      <Route path="/map" element={<Map />} />
-      <Route path="/resources" element={<Resources />} />
-      <Route path="/contacts" element={<Contacts />} />
-      <Route path="/FAQ" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-    </Route>
-  </Routes>
-)
+const AppRoutes: React.FC = () => {
+  useTokenRefresh();
+
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route element={<ProtectedRoute><MapLayout /></ProtectedRoute>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/FAQ" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  )
+}
 
 export default AppRoutes
