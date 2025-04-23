6a7f2af5d3566870626d2f3e90331bebd7b3fe6e
Blondy Martinez <blondymartinezm@gmail.com>
2025-04-24 00:20:56 +0200

    fix: remove unnecessary console logs in getUserLocation function for cleaner output

--- src/utils/getUserLocation.ts
+++ src/utils/getUserLocation.ts
@@ -1,7 +1,6 @@
 export const getUserLocation = (): Promise<[number, number]> => {
   return new Promise((resolve) => {
     if (!navigator.geolocation) {
-      console.log('Geolocation is not supported by this browser. Using default location (Madrid)');
       resolve([40.41672071780762, -3.703395193582358]); // Madrid como fallback
       return;
     }
@@ -12,8 +11,7 @@ export const getUserLocation = (): Promise<[number, number]> => {
         resolve([latitude, longitude]);
       },
       (error) => {
-        console.error('Error getting location:', error);
-        console.log('Unable to get location. Using default location (Madrid).');
+        console.error("Error getting location:", error);
         resolve([40.41672071780762, -3.703395193582358]); // Madrid como fallback
       }
     );
